4fb3c209e9265c6138a9cb5ee57610679a4b4fc3
Shivaprasady <155000804+Shivaprasady@users.noreply.github.com>
2023-12-30 00:07:11 +0530

    Add files via upload

---
+++ main_cpy.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "encode.h"
+#include "types.h"
+
+OperationType check_operation_type(char *argv[])
+{
+    if(argv[1] == NULL)
+    {
+        return e_unsupported;
+    }
+    else if(strcmp(argv[1],"-e") == 0)
+    {
+        return e_encode;
+    }
+    else if(strcmp(argv[1],"-d") == 0)
+    {
+        return e_decode;
+    }
+    else
+    {
+        return e_unsupported;
+    }
+}
+
+Status read_and_validate_encode_args(int argc,char *argv[], EncodeInfo *encInfo)
+{
+   
+    char *temp;
+    int extension_check;
+
+
+    /*Checking 3nd argument is source.bmp file*/
+
+    temp = strstr(argv[2],".bmp");                      //Search for .bmp in 3rd argument
+    if(temp == NULL)                                    //if 3rd argument is invalid (source.bmp)
+    {
+        printf("Error : invalid %s\n",argv[2]);
+        printf("Usage : <file.name>.bmp\n");
+        return e_failure;
+    }
+    extension_check = strcmp(temp,".bmp");              //Comparing string 
+    if(extension_check == 0)                            //3rd argument valid (source.bmp)
+    {
+        encInfo->src_image_fname = argv[2];
+
+        /*Checking 4th argument is secret.txt file */
+
+        temp = strstr(argv[3],".txt");                  //Search for .txt in 4th argument
+        if(temp == NULL)                                //if 4th argument is invalid (secret.txt)
+        {
+            printf("Error : invalid %s\n",argv[3]);
+            printf("Usage : <file.name>.txt\n");
+            return e_failure;
+        }
+        extension_check = strcmp(temp,".txt");
+        if(extension_check == 0)
+        {
+            encInfo->secret_fname = argv[3];
+
+
+            /*checking outputfile name is passed*/ 
+            
+            if(argc == 4)
+            {
+                printf("Output file is not mentioned. Creating Steged_img.bmp as default\n");
+                encInfo->stego_image_fname = "Steged_img.bmp";
+                return e_success;
+            }
+            else
+            {
+                temp = strstr(argv[4],".bmp");
+                if(temp == NULL)
+                {                    
+                    printf("Error : invalid %s\n",argv[4]);
+                    printf("Usage : <file.name>.bmp\n");
+                    return e_failure;
+                }
+                extension_check = strcmp(temp,".bmp");
+                if(extension_check == 0)
+                {
+                    encInfo->stego_image_fname = argv[4];
+                    return e_success;
+                }
+                else
+                {
+                    printf("Error : invalid %s\n",argv[4]);
+                    printf("Usage : <file.name>.bmp\n");
+                    return e_failure;
+                }
+            }
+
+        }
+        else
+        {
+            printf("Error : invalid %s\n",argv[3]);
+            printf("Usage : <file.name>.txt\n");
+        }
+
+
+    }
+    else                                                 //if 3rd argument is invalid (source.bmp)
+    {
+        printf("Error : invalid %s\n",argv[2]);
+        printf("Usage : <file.name>.bmp\n");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    EncodeInfo *encInfo = malloc(sizeof(EncodeInfo));
+
+    uint img_size;
+
+
+        
+    OperationType check = check_operation_type(argv);
+
+    if(check == e_encode)
+    {
+        printf("Encode\n");
+        if(argc > 5 || argc < 4)
+        {
+            printf("Error : Please pass correct arguments\n");
+            printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+            return e_failure;
+        }
+        if(argc >= 4)
+        {
+            Status validate_argu = read_and_validate_encode_args(argc,argv,encInfo);
+
+            if(validate_argu == e_failure)
+            {
+                
+                printf("Error : Please pass correct arguments\n");
+                printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+                return e_failure;
+            }
+
+            /*all arguments passed are valid perform the encoding  */
+            printf("##    Encoding procedure started     ##\n");
+
+        }
+
+    }
+    else if(check == e_decode)
+    {
+        printf("Decode\n");
+    }
+    else if(check == e_unsupported)
+    {
+        printf("Error : Invalid arguments\n");
+        printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+    }
+
+
+    
+}
