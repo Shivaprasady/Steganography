b777bf15015c4067f0e97eb65a12a36feb153bf4
Shivaprasady <155000804+Shivaprasady@users.noreply.github.com>
2024-01-03 00:08:37 +0530

    Add files via upload

--- encode.c
+++ encode.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include<string.h>
 #include "encode.h"
 #include "types.h"
+#include"common.h"
 
 /* Function Definitions */
 
@@ -41,6 +43,91 @@ uint get_file_size(FILE *fptr)
     }
     printf("Done. Not empty\n");
     return size;
+}
+
+
+
+Status read_and_validate_encode_args(int argc,char *argv[], EncodeInfo *encInfo)
+{
+   
+    char *temp;
+    int extension_check;
+
+
+    /*Checking 3nd argument is source.bmp file*/
+
+    temp = strstr(argv[2],".bmp");                      //Search for .bmp in 3rd argument
+    if(temp == NULL)                                    //if 3rd argument is invalid (source.bmp)
+    {
+        printf("Error : invalid %s\n",argv[2]);
+        printf("Usage : <file.name>.bmp\n");
+        return e_failure;
+    }
+    extension_check = strcmp(temp,".bmp");              //Comparing string 
+    if(extension_check == 0)                            //3rd argument valid (source.bmp)
+    {
+        encInfo->src_image_fname = argv[2];
+
+        /*Checking 4th argument is secret.txt file */
+
+        temp = strstr(argv[3],".txt");                  //Search for .txt in 4th argument
+        if(temp == NULL)                                //if 4th argument is invalid (secret.txt)
+        {
+            printf("Error : invalid %s\n",argv[3]);
+            printf("Usage : <file.name>.txt\n");
+            return e_failure;
+        }
+        extension_check = strcmp(temp,".txt");
+        if(extension_check == 0)
+        {
+            encInfo->secret_fname = argv[3];
+
+
+            /*checking outputfile name is passed*/ 
+            
+            if(argc == 4)
+            {
+                printf("Output file is not mentioned. Creating Steged_img.bmp as default\n");
+                encInfo->stego_image_fname = "Steged_img.bmp";
+                return e_success;
+            }
+            else
+            {
+                temp = strstr(argv[4],".bmp");
+                if(temp == NULL)
+                {                    
+                    printf("Error : invalid %s\n",argv[4]);
+                    printf("Usage : <file.name>.bmp\n");
+                    return e_failure;
+                }
+                extension_check = strcmp(temp,".bmp");
+                if(extension_check == 0)
+                {
+                    encInfo->stego_image_fname = argv[4];
+                    return e_success;
+                }
+                else
+                {
+                    printf("Error : invalid %s\n",argv[4]);
+                    printf("Usage : <file.name>.bmp\n");
+                    return e_failure;
+                }
+            }
+
+        }
+        else
+        {
+            printf("Error : invalid %s\n",argv[3]);
+            printf("Usage : <file.name>.txt\n");
+        }
+
+
+    }
+    else                                                 //if 3rd argument is invalid (source.bmp)
+    {
+        printf("Error : invalid %s\n",argv[2]);
+        printf("Usage : <file.name>.bmp\n");
+    }
 }
 
 
@@ -95,16 +182,105 @@ Status check_capacity(EncodeInfo *encInfo)
 {
     /*Get image size */
 
-    encInfo->image_capacity = get_image_size_for_bmp(encInfo->fptr_src_image) - 54; // to ignore the header size included in it.
+    encInfo->image_capacity = get_image_size_for_bmp(encInfo->fptr_src_image);
 
 
     FILE *fptr = encInfo->fptr_secret;
     printf("Checking for %s size\n",encInfo->secret_fname);
     encInfo->secret_file_capacity = get_file_size(fptr);
     printf("Checking for %s capacity to handle %s\n",encInfo->src_image_fname,encInfo->secret_fname);
-    if(encInfo->image_capacity <= encInfo->secret_file_capacity)
+    if(encInfo->image_capacity > (54 + (encInfo->secret_file_capacity + 4 + strlen(MAGIC_STRING) + 4 ) * 8));
     {
-        return e_failure;
+        return e_success;
     }
+    return e_failure;
+}
+
+
+
+/*Data to image*/
+
+Status encode_data_to_image(const char *data, int size, FILE *fptr_src_image, FILE *fptr_stego_image)
+{
+
+    char arr[8];
+    for(int i = 0 ; i < size ; i++)
+    {
+        fread(arr,8,1,fptr_src_image);
+        encode_byte_to_lsb(data[i],arr);
+        fwrite(arr,8,1,fptr_stego_image);
+    }
+
     return e_success;
+}
+
+
+
+/* Encoding bytes to lsb of image bytes*/
+
+Status encode_byte_to_lsb(char data, char *image_buffer)
+{
+    for(int i = 0 , j = 7; i < 8 ; i++,j--)
+    {
+        unsigned char mask = (data >> j) & 1;
+        image_buffer[i] = (image_buffer[i] & ~1) | mask;
+    }
+}
+
+
+
+/* Copying header to destination image */
+
+Status copy_bmp_header(FILE *fptr_src_image,FILE *fptr_stego_image)
+{
+    printf("Copying image header\n");
+
+    char byte;
+    for(int i = 0 ; i < 54 ; i++)
+    {
+        byte = getc(fptr_src_image);
+        putc(byte,fptr_stego_image);
+    }
+
+    printf("Done\n");
+
+    return e_success;
+}
+
+
+/* Copying magic string to destination image */
+Status encode_magic_string(const char *magic_string,EncodeInfo *encInfo)
+{
+    unsigned char byte;
+    int size = strlen(magic_string);
+
+    printf("Encoding magic string signature\n");
+    
+    Status check = encode_data_to_image(magic_string,size,encInfo->fptr_src_image,encInfo->fptr_stego_image);
+
+
+
+    if(check == e_success)
+    {
+        printf("Done\n");
+    }
+}
+
+/*Encode secret files extension */
+
+Status encode_secret_file_extn(const char *file_extn, EncodeInfo *encInfo)
+{
+    int size = strlen(file_extn);
+
+
+    printf("Encoding secret file extension\n");
+
+    Status check = encode_data_to_image(file_extn,size,encInfo->fptr_src_image,encInfo->fptr_stego_image);
+
+
+
+    if(check == e_success)
+    {
+        printf("Done\n");
+    }
 }
---
+++ main_cpy_1.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "encode.h"
+#include "types.h"
+#include"common.h"
+
+OperationType check_operation_type(char *argv[])
+{
+    if(argv[1] == NULL)
+    {
+        return e_unsupported;
+    }
+    else if(strcmp(argv[1],"-e") == 0)
+    {
+        return e_encode;
+    }
+    else if(strcmp(argv[1],"-d") == 0)
+    {
+        return e_decode;
+    }
+    else
+    {
+        return e_unsupported;
+    }
+}
+
+Status do_encoding(EncodeInfo *encInfo)
+{
+    /* opening given files */
+    Status file_ptr = open_files(encInfo);
+    if (file_ptr == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "open_files" );
+        return e_failure;
+    }            
+    else
+    {
+        printf("SUCCESS: %s function completed\n", "open_files" );
+    }
+    
+    
+    /*check capacity */
+    Status capacity = check_capacity(encInfo);
+
+    if(capacity == e_failure)
+    {
+
+        printf("Done. found OKAY\n");
+        printf("%s file is too large to encode in %s \n",encInfo->secret_fname,encInfo->src_image_fname);
+        return e_failure;
+    }
+
+    printf("Done. found OKAY\n");
+
+
+    /*Copying header file*/
+
+    copy_bmp_header(encInfo->fptr_src_image,encInfo->fptr_stego_image);
+
+    /*encoding magic string*/
+
+    encode_magic_string(MAGIC_STRING,encInfo);
+
+    /*Encoding secret file extension */
+
+    const char *file_extn = strstr(encInfo->secret_fname,".");
+    encode_secret_file_extn(file_extn,encInfo);
+
+    return e_success;
+}
+
+int main(int argc, char *argv[])
+{
+
+    EncodeInfo *encInfo = malloc(sizeof(EncodeInfo));
+
+    uint img_size;
+
+
+        
+
+    if(check_operation_type(argv) == e_encode)
+    {
+        printf("Encode\n");
+        if(argc > 5 || argc < 4)
+        {
+            printf("Error : Please pass correct arguments\n");
+            printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+            return e_failure;
+        }
+        if(argc >= 4)
+        {
+
+            Status validate_argu = read_and_validate_encode_args(argc,argv,encInfo);
+
+            if(validate_argu == e_failure)
+            {
+                
+                printf("Error : Please pass correct arguments\n");
+                printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+                return e_failure;
+            }
+
+            /*all arguments passed are valid perform the encoding  */
+            printf("##    Encoding procedure started     ##\n");
+
+            if(do_encoding(encInfo) == e_success)
+            {
+                printf("Encoding done successfully\n");
+            }
+
+
+            
+        }
+
+    }
+    else if(check_operation_type(argv) == e_decode)
+    {
+        printf("Decode\n");
+    }
+    else if(check_operation_type(argv) == e_unsupported)
+    {
+        printf("Error : Invalid arguments\n");
+        printf("usage : ./a.out -e <file_name>.bmp <file_name>.txt [<output_file_name>.bmp]\n");
+    }
+
+
+    
+}
