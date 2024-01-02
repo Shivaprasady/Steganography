dd3050dbffbdcae773264086f57e34db2604b0fe
Shivaprasady <155000804+Shivaprasady@users.noreply.github.com>
2024-01-03 00:14:40 +0530

    Add files via upload

--- main_cpy.c
+++ main_cpy.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include "encode.h"
 #include "types.h"
+#include"common.h"
 
 OperationType check_operation_type(char *argv[])
 {
@@ -21,89 +22,6 @@ OperationType check_operation_type(char *argv[])
     else
     {
         return e_unsupported;
-    }
-}
-
-Status read_and_validate_encode_args(int argc,char *argv[], EncodeInfo *encInfo)
-{
-   
-    char *temp;
-    int extension_check;
-
-
-    /*Checking 3nd argument is source.bmp file*/
-
-    temp = strstr(argv[2],".bmp");                      //Search for .bmp in 3rd argument
-    if(temp == NULL)                                    //if 3rd argument is invalid (source.bmp)
-    {
-        printf("Error : invalid %s\n",argv[2]);
-        printf("Usage : <file.name>.bmp\n");
-        return e_failure;
-    }
-    extension_check = strcmp(temp,".bmp");              //Comparing string 
-    if(extension_check == 0)                            //3rd argument valid (source.bmp)
-    {
-        encInfo->src_image_fname = argv[2];
-
-        /*Checking 4th argument is secret.txt file */
-
-        temp = strstr(argv[3],".txt");                  //Search for .txt in 4th argument
-        if(temp == NULL)                                //if 4th argument is invalid (secret.txt)
-        {
-            printf("Error : invalid %s\n",argv[3]);
-            printf("Usage : <file.name>.txt\n");
-            return e_failure;
-        }
-        extension_check = strcmp(temp,".txt");
-        if(extension_check == 0)
-        {
-            encInfo->secret_fname = argv[3];
-
-
-            /*checking outputfile name is passed*/ 
-            
-            if(argc == 4)
-            {
-                printf("Output file is not mentioned. Creating Steged_img.bmp as default\n");
-                encInfo->stego_image_fname = "Steged_img.bmp";
-                return e_success;
-            }
-            else
-            {
-                temp = strstr(argv[4],".bmp");
-                if(temp == NULL)
-                {                    
-                    printf("Error : invalid %s\n",argv[4]);
-                    printf("Usage : <file.name>.bmp\n");
-                    return e_failure;
-                }
-                extension_check = strcmp(temp,".bmp");
-                if(extension_check == 0)
-                {
-                    encInfo->stego_image_fname = argv[4];
-                    return e_success;
-                }
-                else
-                {
-                    printf("Error : invalid %s\n",argv[4]);
-                    printf("Usage : <file.name>.bmp\n");
-                    return e_failure;
-                }
-            }
-
-        }
-        else
-        {
-            printf("Error : invalid %s\n",argv[3]);
-            printf("Usage : <file.name>.txt\n");
-        }
-
-
-    }
-    else                                                 //if 3rd argument is invalid (source.bmp)
-    {
-        printf("Error : invalid %s\n",argv[2]);
-        printf("Usage : <file.name>.bmp\n");
     }
 }
 
@@ -130,13 +48,24 @@ Status do_encoding(EncodeInfo *encInfo)
 
         printf("Done. found OKAY\n");
         printf("%s file is too large to encode in %s \n",encInfo->secret_fname,encInfo->src_image_fname);
-    }
-    else
-    {
-        printf("Done. found OKAY\n");
+        return e_failure;
     }
 
+    printf("Done. found OKAY\n");
 
+
+    /*Copying header file*/
+
+    copy_bmp_header(encInfo->fptr_src_image,encInfo->fptr_stego_image);
+
+    /*encoding magic string*/
+
+    encode_magic_string(MAGIC_STRING,encInfo);
+
+    /*Encoding secret file extension */
+
+    const char *file_extn = strstr(encInfo->secret_fname,".");
+    encode_secret_file_extn(file_extn,encInfo);
 
     return e_success;
 }
@@ -162,6 +91,7 @@ int main(int argc, char *argv[])
         }
         if(argc >= 4)
         {
+
             Status validate_argu = read_and_validate_encode_args(argc,argv,encInfo);
 
             if(validate_argu == e_failure)
